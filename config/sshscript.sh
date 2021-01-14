544b947b3e4b525e068abcaf5cadab59ba2aa680
RaibeartRuadh <38784332+RaibeartRuadh@users.noreply.github.com>
2021-01-14 11:04:28 +0300

    Delete config directory

--- config/sshscript.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~root/.ssh
-cp ~vagrant/.ssh/auth* ~root/.ssh
-sed -i '65s/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
-sudo yum install -y epel-release
-sudo yum install -y ansible
-sudo yum install -y iperf3
-name=`hostname`
-
